9a52d3bf2300e414ad1becc193b2888ee0978cc4
Brendan Woo <woo.brendan@gmail.com>
2023-11-01 15:48:42 -0400

    use json dumps to make data readable

--- process_responses.py
+++ process_responses.py
@@ -1,10 +1,7 @@
 from fetch_responses import fetch_responses
+import json
 
 entries = fetch_responses()
 
 for entry in entries:
-    print('entry----', entry)
-
-key_list = [
-
-]
+    print('entry----', json.dumps(entry, indent=4))
