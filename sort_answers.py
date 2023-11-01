06b4284f9301f00494360d410161f57c1bdf8030
Brendan Woo <woo.brendan@gmail.com>
2023-11-01 16:18:46 -0400

    add more if conditions to getAnswerValue for choice, email and phone number. in process answer, change the key values to use that function

--- sort_answers.py
+++ sort_answers.py
@@ -1,12 +1,24 @@
+def getAnswerValue(answer):
+    ans_type = answer['type']
+    if ans_type == 'text':
+        return answer['text']
+
+    if ans_type == 'choice':
+        return answer['choice']['label']
+
+    if ans_type == 'phone_number':
+        return answer['phone_number']
+    if ans_type == 'email':
+        return answer['email']
+
+
 # Take in answers array from fetched response
 
-def process_answers(answers_arr):
+def process_answers(answers):
     entry = {
-        'Team': answers_arr[0]['text'],
-        'Season Type': answers_arr[1]['choice']['label']
+        'Team': getAnswerValue(answers[0]),
+        'Season Type': getAnswerValue(answers[1]),
+        'Entrant License Name': getAnswerValue(answers[2]),
+        'Primary Contact': f'{getAnswerValue(answers[3])} + {getAnswerValue(answers[4])}',
+        'Team Contact Phone Number': getAnswerValue(answers[5])
     }
-
-
-def getAnswerValue(answer):
-    if answer['type'] == 'text':
-        return answer['text']
