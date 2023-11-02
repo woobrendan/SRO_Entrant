26de5944b292637dbcb59678c74b778c1f3e128e
Brendan Woo <woo.brendan@gmail.com>
2023-11-02 16:53:07 -0400

    create file to get headers by id. create temp funcs, and create headers dict with id as key and associate header as value

---
+++ sort_answer_id.py
@@ -0,0 +1,14 @@
+# Process Individual Answer object
+def process_answer(answer):
+    pass
+
+
+# Take in answers array from fetched response
+def processAnswerById(answers):
+    pass
+
+
+headers = {
+    "CI2u1enwoqbZ": 'Team',
+    '1rK745G7gN0S': 'Season Type'
+}
