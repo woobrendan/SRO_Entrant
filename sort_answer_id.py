8dfbd1f0ed4ad9a4459f8d31305c08b3d6335dfd
Brendan Woo <woo.brendan@gmail.com>
2023-11-02 17:14:37 -0400

    move headers to top of file, import getAsnwerValue. add logic inside processanswer to get the id, return headers id and get value using getanswer value

--- sort_answer_id.py
+++ sort_answer_id.py
@@ -1,14 +1,21 @@
-# Process Individual Answer object
-def process_answer(answer):
-    pass
-
-
-# Take in answers array from fetched response
-def processAnswerById(answers):
-    pass
-
+from sort_answers import getAnswerValue
 
 headers = {
     "CI2u1enwoqbZ": 'Team',
     '1rK745G7gN0S': 'Season Type'
 }
+
+
+# Process Individual Answer object
+def process_answer(answer):
+    ans_id = answer['field']['id']
+
+    key = headers[ans_id]
+    value = getAnswerValue(answer)
+
+    return {key: value}
+
+
+# Take in answers array from fetched response
+def processAnswerById(answers):
+    pass
