afa8344c66f9cab17a8b2ce27cad57dede21a256
Brendan Woo <woo.brendan@gmail.com>
2023-11-03 18:35:11 -0400

    inside process_asnwer change direct key access to be get and if non return find later. create if function to process the changed data nd print results

--- sort_answer_id.py
+++ sort_answer_id.py
@@ -1,5 +1,7 @@
 from sort_answers import getAnswerValue
 from headers import headers
+from fetch_responses import fetch_responses
+import json
 
 # Process Individual Answer object
 
@@ -7,7 +9,7 @@ from headers import headers
 def process_answer(answer):
     ans_id = answer['field']['id']
 
-    key = headers[ans_id]
+    key = headers.get(ans_id, 'Find later')
     value = getAnswerValue(answer)
 
     return {key: value}
@@ -20,3 +22,16 @@ def processAnswerById(answers):
     for answer in answers:
         val = process_answer(answer)
         entry.update(val)
+
+    return entry
+
+
+if __name__ == '__main__':
+    entries = fetch_responses()
+    all_entries = []
+    for entry in entries:
+        new_entry = processAnswerById(entry['answers'])
+        all_entries.append(new_entry)
+
+    for team in all_entries:
+        print('entry~~~', json.dumps(team, indent=4))
