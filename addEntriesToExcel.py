586eada9b101322463cd452b4580ed7179a2da5b
Brendan Woo <woo.brendan@gmail.com>
2023-11-06 17:01:57 -0500

    create variable first cell, and use as a reference to get styling. add styling settings within loop

--- addEntriesToExcel.py
+++ addEntriesToExcel.py
@@ -2,6 +2,7 @@ import openpyxl
 from fetch_responses import fetch_responses
 from Utility.headers import headers
 from answersToDict import processAllResponses
+from Utility.utility import copy_alignment, copy_border, copy_font
 
 
 def addEntriesToExcel():
@@ -13,11 +14,17 @@ def addEntriesToExcel():
     all_entries = processAllResponses(entries)
 
     first_empty_row = sheet.max_row + 1
+    first_cell = sheet.cell(row=2, column=1)
 
     for entry in all_entries:
         for i, header in enumerate(headers, start=1):
-            sheet.cell(row=first_empty_row, column=i,
-                       value=entry.get(header, ''))
+            new_cell = sheet.cell(row=first_empty_row, column=i)
+            new_cell.value = entry.get(header, '')
+
+            # set formatting of cell
+            new_cell.font = copy_font(first_cell.font)
+            new_cell.alignment = copy_alignment(first_cell.alignment)
+            new_cell.border = copy_border(first_cell.border)
 
         first_empty_row += 1
 
