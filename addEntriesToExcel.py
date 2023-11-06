bb76ce7dd18be2f5aff176f7202078e741a6d9b9
Brendan Woo <woo.brendan@gmail.com>
2023-11-06 16:51:27 -0500

    process the entries with processAlResponses, and update entries being looped

--- addEntriesToExcel.py
+++ addEntriesToExcel.py
@@ -1,5 +1,7 @@
 import openpyxl
 from fetch_responses import fetch_responses
+from Utility.headers import headers
+from answersToDict import processAllResponses
 
 
 def addEntriesToExcel():
@@ -8,10 +10,16 @@ def addEntriesToExcel():
     sheet = wb['Car Registrations']
 
     entries = fetch_responses()
+    all_entries = processAllResponses(entries)
 
-    max_row = sheet.max_row + 1
+    first_empty_row = sheet.max_row + 1
 
-    sheet.cell(row=max_row, column=1, value='Hello')
+    for entry in all_entries:
+        for i, header in enumerate(headers, start=1):
+            sheet.cell(row=first_empty_row, column=i,
+                       value=entry.get(header, ''))
+
+        first_empty_row += 1
 
     wb.save('./Updated/2024 SRO Vehicle Registrations Out.xlsx')
 
