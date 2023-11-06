68509b7efd52e607e2155ce5d11eb0db41f80e30
Brendan Woo <woo.brendan@gmail.com>
2023-11-06 16:38:13 -0500

    fix import for headers to be header_ids, and import from utlity

--- answersToDict.py
+++ answersToDict.py
@@ -1,5 +1,5 @@
 from Utility.sortFuncs import getAnswerValue
-from headers import headers
+from Utility.headers import header_ids
 from fetch_responses import fetch_responses
 import json
 
@@ -8,7 +8,7 @@ import json
 def process_answer(answer):
     ans_id = answer['field']['id']
 
-    key = headers.get(ans_id, '----Find later------')
+    key = header_ids.get(ans_id, '----Find later------')
     value = getAnswerValue(answer)
 
     return {key: value}
@@ -19,6 +19,7 @@ def processAnswersToDict(response):
     entry = {}
     entry['id'] = response['response_id']
     entry['Submitted_at'] = response['submitted_at']
+    entry['2024 Confirmed Number'] = ''
 
     for answer in response['answers']:
         val = process_answer(answer)
@@ -26,9 +27,8 @@ def processAnswersToDict(response):
 
     return entry
 
+
 # pass in param of all newest responses and process all
-
-
 def processAllResponses(responses):
     all_entries = []
 
