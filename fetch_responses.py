90a79d7cd974acd33eeef2d7962a869cf69aae16
Brendan Woo <woo.brendan@gmail.com>
2023-11-07 17:17:51 -0500

    update latest fetch, add in datestring to url fetch

--- fetch_responses.py
+++ fetch_responses.py
@@ -5,7 +5,7 @@ import os
 load_dotenv()
 
 # Last upload - update with each get
-date = '2023-11-01T19:13:49.643Z'
+date = '2023-11-07T22:17:09.613Z'
 
 date_str = f'?since={date}'
 page_size = f'?page_size=100'
@@ -15,7 +15,7 @@ def fetch_responses():
     form_id = os.environ.get('FORM')
     token = os.environ.get('TOKEN')
 
-    url = f'https://api.typeform.com/forms/{form_id}/responses'
+    url = f'https://api.typeform.com/forms/{form_id}/responses{date_str}'
 
     headers = {
         'Authorization': f'Bearer {token}'
