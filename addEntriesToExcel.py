482dd873585ed546c4d933daaf8a317ce6cea78d
Brendan Woo <woo.brendan@gmail.com>
2023-11-09 15:41:49 -0500

    replace all logic for writing to excel with addToSheet function call. rermove old imports

--- addEntriesToExcel.py
+++ addEntriesToExcel.py
@@ -1,8 +1,7 @@
 import openpyxl
 from fetch_responses import fetch_responses
-from Utility import headers, gr_headers
 from answersToDict import processAllResponses
-from Utility.utility import copy_alignment, copy_border, copy_font, findFirstEmptyRow, getAllId
+from Utility.addToSheet import addToSheet
 
 
 def addEntriesToExcel(date_str, series):
@@ -18,26 +17,7 @@ def addEntriesToExcel(date_str, series):
         return
     all_entries = processAllResponses(entries, series)
 
-    # add filter to check if entry id exists in the sheet already
-    first_empty_row = findFirstEmptyRow(sheet)
-    first_cell = sheet.cell(row=2, column=1)
-
-    header_title = gr_headers.headers if series == 'GR' else headers.headers
-    existing_ids = getAllId(sheet, series)
-
-    for entry in all_entries:
-        if entry['id'] in existing_ids:
-            continue
-        for i, header in enumerate(header_title, start=1):
-            new_cell = sheet.cell(row=first_empty_row, column=i)
-            new_cell.value = entry.get(header, '')
-
-            # set formatting of cell
-            new_cell.font = copy_font(first_cell.font)
-            new_cell.alignment = copy_alignment(first_cell.alignment)
-            new_cell.border = copy_border(first_cell.border)
-
-        first_empty_row += 1
+    addToSheet(sheet, series, all_entries)
 
     wb.save(f'./Updated/2024 {excel_doc} Vehicle Registrations Out.xlsx')
 
@@ -49,5 +29,5 @@ if __name__ == '__main__':
     addEntriesToExcel(date_str=gr_date, series='GR')
 
     # fetch SRO
-    sro_date = '2023-11-07T18:11:35Z'
+    sro_date = '2023-10-07T18:11:35Z'
     addEntriesToExcel(sro_date, series='SRO')
