ecb441e5deb62bf495c3de0f1fe08b6edbb0afdd
Brendan Woo <woo.brendan@gmail.com>
2023-11-09 15:41:09 -0500

    create file to house logic of writing to excel, move from addEntriestoexcel

---
+++ Utility/addToSheet.py
@@ -0,0 +1,25 @@
+from Utility.utility import findFirstEmptyRow, getAllId, copy_alignment, copy_border, copy_font
+from Utility import headers, gr_headers
+
+
+def addToSheet(sheet, series, all_entries):
+    # add filter to check if entry id exists in the sheet already
+    first_empty_row = findFirstEmptyRow(sheet)
+    first_cell = sheet.cell(row=2, column=1)
+
+    header_title = gr_headers.headers if series == 'GR' else headers.headers
+    existing_ids = getAllId(sheet, series)
+
+    for entry in all_entries:
+        if entry['id'] in existing_ids:
+            continue
+        for i, header in enumerate(header_title, start=1):
+            new_cell = sheet.cell(row=first_empty_row, column=i)
+            new_cell.value = entry.get(header, '')
+
+            # set formatting of cell
+            new_cell.font = copy_font(first_cell.font)
+            new_cell.alignment = copy_alignment(first_cell.alignment)
+            new_cell.border = copy_border(first_cell.border)
+
+        first_empty_row += 1
