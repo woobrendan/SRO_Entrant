2b01acc483c4bacbb7369335914b290c01cf9f3b
Brendan Woo <woo.brendan@gmail.com>
2023-11-10 12:32:45 -0500

    import getMostRecentdate, pass that val to fetch responses. remove date str param from addEntriesToexcel, and rremove dates fromr main block

--- addEntriesToExcel.py
+++ addEntriesToExcel.py
@@ -3,22 +3,25 @@ from Utility.fetch_responses import fetch_responses
 from Utility.answersToDict import processAllResponses
 from Utility.addToCarReg import addToCarReg
 from Utility.sortFuncs import filterEntriesById
-from Utility.utility import getAllId
+from Utility.utility import getAllId, getMostRecentDate
 
 
-def addEntriesToExcel(date_str, series):
+def addEntriesToExcel(series):
     excel_doc = 'GR Cup' if series == 'GR' else 'SRO'
     wb = openpyxl.load_workbook(
         f'./templates/2024 {excel_doc} Vehicle Registrations.xlsx')
 
     sheet = wb['Car Registrations']
 
+    recent_date = getMostRecentDate(sheet, series)
     existing_ids = getAllId(sheet, series)
 
     # Fetch respones exit if no new respones, if responses process them to own dict, then filter removing duplicate ids
-    entries = fetch_responses(date_str, series)
+    entries = fetch_responses(recent_date, series)
+
     if len(entries) == 0:
         return
+
     all_entries = processAllResponses(entries, series)
     filtered_entries = filterEntriesById(existing_ids, all_entries)
 
@@ -31,9 +34,7 @@ def addEntriesToExcel(date_str, series):
 if __name__ == '__main__':
 
     # fetch GR
-    gr_date = '2023-11-07T18:05:04Z'
-    addEntriesToExcel(date_str=gr_date, series='GR')
+    addEntriesToExcel(series='GR')
 
     # fetch SRO
-    sro_date = '2023-10-07T18:11:35Z'
-    addEntriesToExcel(sro_date, series='SRO')
+    addEntriesToExcel(series='SRO')
