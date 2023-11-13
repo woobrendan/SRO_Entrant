90c85c2b4ceb4dac9cbb5c6812a9c1b21bf57a31
Brendan Woo <woo.brendan@gmail.com>
2023-11-13 13:18:21 -0500

    create function addValueToExcel to reuse throughout, and follow logic to place values into excel

--- Utility/utility.py
+++ Utility/utility.py
@@ -63,3 +63,20 @@ def getMostRecentDate(sheet, series):
     most_recent = max(date_objs)
 
     return most_recent.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+
+def addValuesToExcel(headers, entries, sheet):
+    first_row = findFirstEmptyRow(sheet)
+    first_cell = sheet.cell(row=2, column=1)
+
+    for entry in entries:
+        for i, header in enumerate(headers, start=1):
+            new_cell = sheet.cell(row=first_row, column=i)
+            new_cell.value = entry.get(header, '')
+
+            # set formatting of cell
+            new_cell.font = copy_font(first_cell.font)
+            new_cell.alignment = copy_alignment(first_cell.alignment)
+            new_cell.border = copy_border(first_cell.border)
+
+        first_row += 1
