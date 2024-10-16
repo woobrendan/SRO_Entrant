0b4b5fe81d595327c0727b2d893ba1be660a18be
Brendan Woo <woo.brendan@gmail.com>
2024-10-16 12:48:32 -0700

    add in func for current year, and new wb for 2025

--- addEntriesToExcel.py
+++ addEntriesToExcel.py
@@ -1,4 +1,5 @@
 import openpyxl
+from datetime import datetime
 from Utility.fetch_responses import fetch_responses
 from Utility.answersToDict import processAllResponses
 from Utility.sortFuncs import filterEntriesById
@@ -9,8 +10,13 @@ from Utility import headers, gr_headers
 
 def addEntriesToExcel(series):
     excel_doc = 'GR Cup' if series == 'GR' else 'SRO'
-    wb = openpyxl.load_workbook(
-        f'../2024 {excel_doc} Vehicle Registrations.xlsx')
+    current_year = datetime.now().year
+
+    wb = openpyxl.load_workbook(f'./2025/ 2025 {excel_doc} Vehicle Registrations.xlsx')
+
+
+    # wb = openpyxl.load_workbook(
+    #     f'../2024 {excel_doc} Vehicle Registrations.xlsx')
 
     sheet = wb['Car Registrations']
 
