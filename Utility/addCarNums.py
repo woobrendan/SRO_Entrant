fc56831425ea0521be939598cffc6475edc230c9
Brendan Woo <woo.brendan@gmail.com>
2024-10-25 12:36:24 -0700

    handle logic for mclaren, update else block to dynamically handle gr vs mclaren

--- Utility/addCarNums.py
+++ Utility/addCarNums.py
@@ -1,5 +1,6 @@
-from Utility.gr_headers import num_headers
-from Utility.headers import sro_num_headers
+from headers.gr_headers import gr_num_headers
+from headers.headers import sro_num_headers
+from headers.mcl_headers import mcl_num_headers
 from Utility.sortFuncs import sortEntryNumberTracking
 from Utility.utility import addValuesToExcel
 
@@ -7,16 +8,19 @@ from Utility.utility import addValuesToExcel
 def addCarNums(wb, series, all_entries):
     entryDict = sortEntryNumberTracking(series, all_entries)
 
-    if series == 'GR Cup':
-        sheet = wb['GR Nums']
-
-        entries = entryDict['GR Nums']
-
-        addValuesToExcel(num_headers, entries, sheet)
-
     # handle SRO number tracking
-    else:
+    if series == 'SRO':
         for key, entries in entryDict.items():
             sheet = wb[key]
 
             addValuesToExcel(sro_num_headers, entries, sheet)
+    
+    # handle GR and McLaren Nums
+    else:
+        seriesTab = 'GR Nums' if series == 'GR Cup' else 'MCL Nums'
+        num_head = gr_num_headers if series == 'Gr Cup' else mcl_num_headers
+        sheet = wb[seriesTab]
+
+        entries = entryDict[seriesTab]
+
+        addValuesToExcel(num_head, entries, sheet)
