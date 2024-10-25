13f19e58e3cc1062f99913b9fac86777bbfdcdb9
Brendan Woo <woo.brendan@gmail.com>
2024-10-25 12:33:43 -0700

    rejig sort entry num to use object and get for series arrays

--- Utility/sortFuncs.py
+++ Utility/sortFuncs.py
@@ -13,32 +13,28 @@ def getAnswerValue(answer):
 
 # takes in array of proccessed entries, sorts for number tracking
 def sortEntryNumberTracking(series, all_entries):
-    entries = {}
 
-    ## Create key in entries object for each series numbers
-    if series == 'SRO':
-        entries['GT Nums'] = []
-        entries['TC Nums'] = []
-
-    if series == 'GR Cup':
-        entries['GR Nums'] = []
-
-    if series == 'McLaren':
-        entries['MCL Nums'] = []
+    ## Create key in entries object for each series numbers, fetch appropriate series based on prop
+    entries = {
+        "SRO": {'GT Nums': [], 'TC Nums': []},
+        'GR Cup': {'GR Nums': []},
+        'McLaren': {"MCL Nums": []}
+    }.get(series, {})
 
     ## loop through the entry array and place numbers in approproate series
     for entry in all_entries:
         if series == 'GR Cup':
             entries['GR Nums'].append(entry)
 
-        if series == 'McLaren':
+        elif series == 'McLaren':
             entries['MCL Nums'].append(entry)
 
-        if entry['Series'] == 'TC America':
-            entries['TC Nums'].append(entry)
+        elif series == 'SRO':
+            if entry.get('Series') == 'TC America': 
+                entries['TC Nums'].append(entry)
 
-        else:
-            entries['GT Nums'].append(entry)
+            else:
+                entries['GT Nums'].append(entry)
 
     return entries
 
