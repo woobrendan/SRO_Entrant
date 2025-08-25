385ff75b24d06b1888814f0244db11d0bf667906
Brendan Woo <87395076+woobrendan@users.noreply.github.com>
2025-08-25 18:41:05 -0400

    update processallresponses to take variable year, pass year to other functions, update process_answer to handle year for headesr

--- Utility/answersToDict.py
+++ Utility/answersToDict.py
@@ -1,44 +1,55 @@
 from Utility.sortFuncs import getAnswerValue
-from headers import gr_headers, headers, mcl_headers
+from headers import gr_headers, headers, mcl_headers, headers_2026
 from Utility.fetch_responses import fetch_responses
 import json
 
 # Process Individual Answer object
-def process_answer(answer, series):
+def process_answer(answer, series, year):
     ans_id = answer['field']['id']
-    if series == 'GR Cup':
-        header_vals = gr_headers.header_ids
-    elif series == 'McLaren':
-        header_vals = mcl_headers.header_ids
-    else:
-        header_vals = headers.header_ids    
 
-    key = header_vals.get(ans_id, '----Find later------')
+    #update for 2026 when year changes
+    series_headers = {
+        "GR Cup": {"2025": gr_headers.header_ids, "2026": gr_headers.header_ids},
+        "McLaren": {"2025": mcl_headers.header_ids, "2026": mcl_headers.header_ids},
+        "SRO": {"2025": headers.header_ids, "2026": headers_2026.header_ids}
+    }
+
+    # if series == 'GR Cup':
+    #     header_vals = gr_headers.header_ids
+    # elif series == 'McLaren':
+    #     header_vals = mcl_headers.header_ids
+    # else:
+    #     header_vals = headers.header_ids    
+
+    # key = header_vals.get(ans_id, '----Find later------')
+    year_key = year if year == "2026" else "2025"
+    all_headers = series_headers[series][year_key]
+    key = all_headers.get(ans_id, '----Find later------')
     value = getAnswerValue(answer)
 
     return {key: value}
 
 
 # Take in answers array from fetched response. This is a single entrant report
-def processAnswersToDict(response, series):
+def processAnswersToDict(response, series, year):
     entry = {}
     entry['id'] = response['response_id']
     entry['Submitted_at'] = response['submitted_at']
     entry['2024 Confirmed Number'] = ''
 
     for answer in response['answers']:
-        val = process_answer(answer, series)
+        val = process_answer(answer, series, year)
         entry.update(val)
 
     return entry
 
 
 # pass in param of all newest responses and process all
-def processAllResponses(responses, series):
+def processAllResponses(responses, series, year):
     all_entries = []
 
     for response in responses:
-        new_response = processAnswersToDict(response, series)
+        new_response = processAnswersToDict(response, series, year)
         all_entries.append(new_response)
 
     return all_entries
